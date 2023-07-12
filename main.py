39a2fe0c667afcc13e04993855888732460e06b9
PandaCodingPy <139103491+PandaCodingPy@users.noreply.github.com>
2023-07-12 17:12:11 +0800

    Update main.py in main branch

--- main.py
+++ main.py
@@ -6,4 +6,26 @@ def decoder(code):
 
 class App:
   def __init__(self, name):
+    self.name = name
+    self.code = ''
+
+  def name(self):
+    return self.name
+
+  def run(self, structure):
+    finish_code = decoder(structure)
+    return finish_code
+
+class Structure:
+  def __init__(self):
+    self.code = []
+    self.type = ['text', 'button']
+
+  def add(self, type, value):
+    if type in self.type
+    self.code.append(type + '-' + value)
+
+  def code(self):
+    return self.code
+    
     
